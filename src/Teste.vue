736e897bd3e589d8aea2850fba4a9ac3b2793433
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-10-16 19:39:16 -0300

    init

---
+++ src/Teste.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    name: {{ name }}
+    lastName: {{ lastName }}
+  </div>
+</template>
+
+<script>
+import VueCoeProp from './lib'
+
+export default {
+  name: 'teste',
+
+  mixins: [ VueCoeProp ],
+
+  props: {
+    name: {
+      type: String
+      // coeValidator: function (value) {
+      //   console.log('this: ', this)
+      //   return true
+      // }
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      validator: v => console.log('thissssss', this) || !!v,
+      // validator: v => !!v,
+      default: () => ''
+    }
+  },
+
+  data () {
+    return {
+      dataType: false
+    }
+  }
+}
+</script>
---
+++ src/lib/index.js
@@ -0,0 +1,26 @@
+export default {
+  created () {
+    let { props } = this.$options
+
+    if (!props) return
+
+    console.log('before', props)
+    console.log('-----')
+
+    props = Object
+      .entries(props)
+      .map(([ key, value ]) => {
+        return Object.entries(value).reduce((newProps, [ type, fn ]) => {
+          newProps[key] = {
+            ...newProps[key],
+            [type]: typeof fn === 'function' ? props[key][type].bind(this, type === 'validator' && this.$props[key]) : props[key][type]
+          }
+
+          return newProps
+        }, {})
+      })
+
+    console.log('-----')
+    console.log('after', props)
+  }
+}
