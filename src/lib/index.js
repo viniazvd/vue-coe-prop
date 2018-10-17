257535b425f70e3561013dacfab3a24572f424e8
Vinicius Azevedo <viniazvd@gmail.com>
2018-10-17 01:07:00 -0300

    .

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
+// import Vue from 'vue'
+
 export default {
-  created () {
+  beforeCreate () {
     let { props } = this.$options
 
     if (!props) return
@@ -9,18 +11,34 @@ export default {
 
     props = Object
       .entries(props)
-      .map(([ key, value ]) => {
-        return Object.entries(value).reduce((newProps, [ type, fn ]) => {
+      .reduce((acc, [ key, value ]) => {
+        const bounded = Object.entries(value).reduce((newProps, [ type, fn ]) => {
           newProps[key] = {
             ...newProps[key],
-            [type]: typeof fn === 'function' ? props[key][type].bind(this, type === 'validator' && this.$props[key]) : { ...props[key][type] }
+            [type]: typeof fn === 'function' ? props[key][type].bind(this) : { ...props[key][type] }
+            // [type]: typeof fn === 'function' ? props[key][type].call(this, type === 'validator' && this.$props[key]) : { ...props[key][type] }
           }
 
           return newProps
         }, {})
-      })
+
+        acc = { ...acc, ...bounded }
+
+        return acc
+      }, {})
 
     console.log('-----')
     console.log('after', props)
+
+    // Vue.config.optionMergeStrategies.COE = function (toVal, fromVal) {
+    //   console.log('DADSD', toVal, fromVal)
+
+    //   return { ...vueCoeProps }
+    // }
+
+    // let strategies = Vue.config.optionMergeStrategies
+    // strategies.props = strategies.COE
+
+    // Vue.util.mergeOptions(this.$options.props, ...vueCoeProps)
   }
 }
